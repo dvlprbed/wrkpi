9e783a964db548bb410dee1783020ab7b7576b43
amawork <projetminfo@gmail.com>
2016-12-14 00:41:10 +0100

    graph class + value in database

---
+++ app/src/main/java/lolu/wrkmbd/Graph.java
@@ -0,0 +1,11 @@
+package lolu.wrkmbd;
+
+import android.content.Context;
+import android.view.View;
+
+
+public class Graph extends View {
+    public Graph(Context context) {
+        super(context);
+    }
+}
