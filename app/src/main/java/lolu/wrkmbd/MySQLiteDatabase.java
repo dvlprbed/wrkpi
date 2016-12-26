494aa6c8398dc32d541cb03e17fccbb4fda74149
amawork <projetminfo@gmail.com>
2016-12-27 00:51:24 +0100

    maj db

--- app/src/main/java/lolu/wrkmbd/MySQLiteDatabase.java
+++ app/src/main/java/lolu/wrkmbd/MySQLiteDatabase.java
@@ -1,9 +1,14 @@
 package lolu.wrkmbd;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by hp on 23/11/2016.
@@ -20,6 +25,7 @@ public class MySQLiteDatabase extends SQLiteOpenHelper {
 
     // Books Table Columns names
     private static final String KEY_ID = "valeur";
+    private static final String KEY_VALUEMBED = "valeur_mbed";
 
     public MySQLiteDatabase(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -28,7 +34,7 @@ public class MySQLiteDatabase extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         String CREATE_POSITION_TABLE = "CREATE TABLE "+TABLE_POSITIONS+" ( " +
-                KEY_ID+ " INTEGER PRIMARY KEY AUTOINCREMENT )";
+                KEY_ID+ " INTEGER PRIMARY KEY AUTOINCREMENT"+ KEY_VALUEMBED + " INTEGER" + ")";
 
         Log.d("OnCreate",CREATE_POSITION_TABLE);
         // create books table
@@ -36,6 +42,55 @@ public class MySQLiteDatabase extends SQLiteOpenHelper {
 
     }
 
+
+
+    void addValueMbed(MbedModel mbdModel) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(KEY_VALUEMBED, mbdModel.getValueMbed());
+
+        // Inserting Row
+        db.insert(TABLE_POSITIONS, null, values);
+        db.close(); // Closing database connection
+    }
+
+    MbedModel getMbedValue(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(TABLE_POSITIONS, new String[] { KEY_ID,
+                        KEY_VALUEMBED }, KEY_ID + "=?",
+                new String[] { String.valueOf(id) }, null, null, null, null);
+        if (cursor != null)
+            cursor.moveToFirst();
+
+        MbedModel mbedModel = new MbedModel(Integer.parseInt(cursor.getString(0)),Integer.parseInt(cursor.getString(1)));
+        // return contact
+        return mbedModel;
+    }
+
+    public List<MbedModel> getAllMbedValues() {
+        List<MbedModel> mbedValueList = new ArrayList<MbedModel>();
+        // Select All Query
+        String selectQuery = "SELECT  * FROM " + TABLE_POSITIONS;
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        // looping through all rows and adding to list
+        if (cursor.moveToFirst()) {
+            do {
+                MbedModel mbedValue = new MbedModel();
+                mbedValue.setId(Integer.parseInt(cursor.getString(0)));
+                mbedValue.setValueMbed(Integer.parseInt(cursor.getString(0)));
+
+                mbedValueList.add(mbedValue);
+            } while (cursor.moveToNext());
+        }
+
+
+        return mbedValueList;
+    }
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
         db.execSQL("DROP TABLE IF EXISTS "+TABLE_POSITIONS);
