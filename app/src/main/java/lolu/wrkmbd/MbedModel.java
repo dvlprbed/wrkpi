8d5029e622d4a880fd282894e1d160f8192601b8
amawork <projetminfo@gmail.com>
2016-12-26 01:35:50 +0100

    maj model

---
+++ app/src/main/java/lolu/wrkmbd/MbedModel.java
@@ -0,0 +1,30 @@
+package lolu.wrkmbd;
+
+
+
+/**
+ * Created by hp on 23/11/2016.
+ */
+
+public class MbedModel {
+    public int id;
+    public int valueMbed;
+
+    public MbedModel(){
+        super();
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getValueMbed() {
+        return valueMbed;
+    }
+
+    public void setValueMbed(int valueMbed) {
+        this.valueMbed = valueMbed;
+    }
+}
